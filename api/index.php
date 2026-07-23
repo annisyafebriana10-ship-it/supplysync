f081422d081c509ba188149c82ab54f970a8dad3
Annisyafebriana <annisyafebriana10@gmail.com>
2026-07-23 14:17:41 +0700

    Ultimate Vercel Hack: Move storage to /tmp and rescue Firebase

--- api/index.php
+++ api/index.php
@@ -1,4 +1,15 @@
 <?php
 
-// Jembatan untuk mengarahkan Vercel (serverless) ke sistem utama Laravel
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Trik Khusus Vercel: Alihkan folder storage ke /tmp
+$app->useStoragePath('/tmp');
+
+// Jalankan aplikasi
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
